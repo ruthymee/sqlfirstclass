71aa6f424595abc48d32799de79cd7fa9084033c
ruthymee <ruthymee@gmail.com>
2024-09-14 14:15:01 +0100

    SQL_QUERY_JOINING

--- sqlfile.sql
+++ sqlfile.sql
@@ -2,12 +2,12 @@
 --select the table to view the attributes in the in the entities
 SELECT * FROM sales_rescord
 
+product_record AS p ON s.product_id=p.P
 SELECT * FROM product_record
 
 SELECT customer, units_sold, product_name, price, s.product_id
 FROM sales_rescord AS s 
-JOIN
-product_record AS p ON s.product_id=p.Product_id
+JOINroduct_id
 
 --CALCULATE THE AMOUNT EARN IN EACH TRANSACTION AND ORDER
 SELECT customer, units_sold, price, production_cost, (units_sold * price) AS Selling_price,
